e371994198288a1f9a69ed403cd34d9ae80b35aa
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-07-03 20:40:42 +0500

    refactor

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "../src/App-memo";
+import AppV2 from "../src/App-v2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppV2 />
   </React.StrictMode>
 );
