aadbadab519e9f952a582bf344ef68e2d5fa0aeb
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-07-03 15:18:44 +0500

    feat: optimization trick with children props

--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { Children, useState } from "react";
 
 function SlowComponent() {
-  // If this is too slow on your maching, reduce the `length`
-  const words = Array.from({ length: 100_000 }, () => "WORD");
+  // If this is too slow on your matching, reduce the `length`
+  const words = Array.from({ length: 100 }, () => "WORD");
   return (
     <ul>
       {words.map((word, i) => (
@@ -14,13 +14,36 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+
+      {children}
     </div>
   );
 }
+
+export default function Test() {
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
+
+  return (
+    <div>
+      <Counter>
+        <SlowComponent />
+      </Counter>
+    </div>
+  );
+}
+
+//! NOTE
+// When React comes to 'Test' component, it immediately creates 'SlowComponent' (as it's children prop of 'Counter') before re-render of 'Counter' component. When 'Counter' re-renders, it doesn't affect on its children props. This is some kind of optimization trick by passing element as children props.
